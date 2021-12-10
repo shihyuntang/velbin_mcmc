cdf7108520e0e41bafd3c2ea44f68d8723c6bc8c
sytang <sytang@lowell.edu>
2021-12-09 17:35:46 -0700

    test

--- function/mcmc_func.py
+++ function/mcmc_func.py
@@ -139,4 +139,5 @@ if __name__ == "__main__":
         ndim = 4
         fn = "pmdec_mcmcsave.h5"
     
+    print(fn, args.burnin, ndim, 20, png_save_name, partyp)
     cornerplot(fn, args.burnin, ndim, 20, png_save_name, partype)
